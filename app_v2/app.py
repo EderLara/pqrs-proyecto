5b5ff60e8a8d6400e82009fa42fb1e56f4902f27
Eder Lara T <ederlatru@gmail.com>
2025-12-08 10:43:26 -0500

    Mejora visual en entrenamiento

--- app_v2/app.py
+++ app_v2/app.py
@@ -340,7 +340,7 @@ def main_app():
         
         st.markdown("### ⚙️ Configuración del Modelo")
         
-        col1, col2, col3 = st.columns([2, 1, 1])
+        col1, col2, col3 = st.columns([2, 1, 1], vertical_alignment='center')
         
         with col1:
             version_name = st.text_input(
